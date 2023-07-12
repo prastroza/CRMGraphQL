363d4f9cb1b148dd3a7bf6a6650a977335baec8e
Patricio <pastroza@gmail.com>
2023-07-12 14:38:56 -0400

    Cambios en indez.js

--- index.js
+++ index.js
@@ -35,6 +35,9 @@ const server = new ApolloServer({
 });
 
 //Arrancar el servidor
-server.listen({port:process.env.PORT || 4000}).then( ({url}) => {
+
+//server.listen({port:process.env.PORT || 4000}).then( ({url}) => {
+const port = process.env.PORT || 4000;
+server.listen({port}).then( ({url}) => {    
     console.log( `Servidor listo en la URL ${url}`)
 })
